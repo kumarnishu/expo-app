900aa95b457a363f52964b523a518722a816a96d
unknown <kumarnishu437@gmail.com>
2023-11-29 13:19:01 +0530

    first project official expo

---
+++ ImageViewer.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Image } from 'react-native';
+
+export default function ImageViewer({ placeholderImageSource, selectedImage }: { placeholderImageSource: any, selectedImage: any }) {
+
+    const imageSource = selectedImage ? selectedImage : placeholderImageSource
+    return (
+        <Image source={imageSource} style={styles.image} />
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    }
+});
